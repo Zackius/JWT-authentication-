1416d76f62bd7ea543fb8c9a91690fc3098a040e
Zacharia <ndunguzachary24@gmail.com>
2022-11-19 23:08:09 +0300

    first commit

---
+++ app/lib/json_web_token.rb
@@ -0,0 +1,14 @@
+class JsonWebToken
+SECRET_KEY = Rails.application.secrets.secret_key_base.  to_s
+
+def self.encode(playload, exp =24.hours.from_now)
+    playload[:exp] =exp.to_i
+    JWT.encode(playload, SECRET_KEY)
+end
+
+def self.decode(token)
+decoded = JWT.decode(token, SECRET_KEY)[0]
+HashWithIndifferentAccess.new decoded
+end
+
+end
